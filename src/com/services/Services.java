087f056ef396024f8fa28cc9b33ea3f37d74cdbc
osama elsawy <osama elsawy>
2016-03-27 09:09:28 -0700

    first upload

---
+++ src/com/services/Services.java
@@ -0,0 +1,141 @@
+package com.services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.mvc.Viewable;
+import org.json.simple.JSONObject;
+
+import com.models.DBConnection;
+import com.models.UserModel;
+
+@Path("/")
+public class Services {
+
+	/*
+	 * @GET
+	 * 
+	 * @Path("/signup")
+	 * 
+	 * @Produces(MediaType.TEXT_HTML) public Response signUp(){ return
+	 * Response.ok(new Viewable("/Signup.jsp")).build(); }
+	 */
+
+	@POST
+	@Path("/signup")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String signUp(@FormParam("name") String name,
+			@FormParam("email") String email, @FormParam("pass") String pass) {
+		UserModel user = UserModel.addNewUser(name, email, pass);
+		JSONObject json = new JSONObject();
+		json.put("id", user.getId());
+		json.put("name", user.getName());
+		json.put("email", user.getEmail());
+		json.put("pass", user.getPass());
+		json.put("lat", user.getLat());
+		json.put("long", user.getLon());
+		return json.toJSONString();
+	}
+
+	@POST
+	@Path("/login")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String login(@FormParam("email") String email,
+			@FormParam("pass") String pass) {
+		UserModel user = UserModel.login(email, pass);
+		JSONObject json = new JSONObject();
+		json.put("id", user.getId());
+		json.put("name", user.getName());
+		json.put("email", user.getEmail());
+		json.put("pass", user.getPass());
+		json.put("lat", user.getLat());
+		json.put("long", user.getLon());
+		return json.toJSONString();
+	}
+	
+	@POST
+	@Path("/updatePosition")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updatePosition(@FormParam("id") String id,
+			@FormParam("lat") String lat, @FormParam("long") String lon) {
+		Boolean status = UserModel.updateUserPosition(Integer.parseInt(id), Double.parseDouble(lat), Double.parseDouble(lon));
+		JSONObject json = new JSONObject();
+		json.put("status", status ? 1 : 0);
+		return json.toJSONString();
+	}
+	
+	@POST
+	@Path("/follow")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String follow(@FormParam("usermail") String umail,
+			@FormParam("follower") String follower_mail) {
+		boolean user = UserModel.follow(umail, follower_mail);
+		JSONObject json = new JSONObject();
+		json.put("Value", user);
+		
+		return json.toJSONString();
+	}
+	
+	@POST
+	@Path("/unfollow")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String unfollow(@FormParam("usermail") String umail,
+			@FormParam("follower") String follower_mail) {
+		boolean user = UserModel.unfollow(umail, follower_mail);
+		JSONObject json = new JSONObject();
+		json.put("Value", user);
+		
+		return json.toJSONString();
+	}
+	
+	@POST
+	@Path("/getfollower")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getfollower(@FormParam("usermail") String umail) {
+		ArrayList<String> user = UserModel.getfollower(umail);
+		JSONObject json = new JSONObject();
+		for(int i=0;i<user.size();i++){
+			json.put("Value", user.get(i));	
+		}
+		
+		return json.toJSONString();
+	}
+	
+	@POST
+	@Path("/getuserlastposition")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getuserlastposition(@FormParam("usermail") String umail) {
+		String user = UserModel.getuserlastposition(umail);
+		JSONObject json = new JSONObject();
+ 			json.put("Value", user);	
+	
+		
+		return json.toJSONString();
+	}
+
+
+
+
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getJson() {
+		return "Hello after editing";
+		// Connection URL:
+		// mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/
+	}
+	
+}
